26c7e01f30750b966089526c2a7e8efb5cb7e1ee
Mark Nolan <marknolandev@gmail.com>
2018-08-21 11:06:22 +0100

    Add files via upload

--- js/freelancer.js
+++ js/freelancer.js
@@ -3,7 +3,7 @@
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
@@ -36,5 +36,10 @@
       $(this).removeClass("floating-label-form-group-with-focus");
     });
   });
+
+    //Tooltip
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 
 })(jQuery); // End of use strict
